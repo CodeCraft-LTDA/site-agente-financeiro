3b768dde32eb7531d611bcfdb53dc52cb0ab3b9c
Michele Rocha <michele.rocha@grad.ufsc.br>
2024-03-08 12:13:30 -0300

    #Fix: Profile 😉

--- core/settings.py
+++ core/settings.py
@@ -119,8 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates/static')]
-STATIC_URL = 'static/'
-STATIC_ROOT = '/static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
