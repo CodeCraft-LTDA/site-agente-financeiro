416389850fe1e7ade816ce67a6190edda67b0eaa
Ubuntu <ubuntu@ip-172-31-36-129.sa-east-1.compute.internal>
2024-03-10 13:31:40 +0000

    adjust: arrumado commit do git

--- core/settings.py
+++ core/settings.py
@@ -127,3 +127,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS=['https://*.analyticorp.com.br', 'https://www.analyticorp.com.br', 'https://analyticorp.com.br:8000']
